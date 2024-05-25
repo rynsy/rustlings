519d71d77411016f3bb0ae2828a6eb6644833a30
Ryan Lindsey <ryan.lindsey44@gmail.com>
2024-05-24 20:03:10 -0400

    session 1

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -5,17 +5,15 @@
 // Example: England,France,4,2 (England scored 4 goals, France 2).
 //
 // You have to build a scores table containing the name of the team, the total
-// number of goals the team scored, and the total number of goals the team 
-// conceded. One approach to build the scores table is to use a Hashmap. 
-// The solution is partially written to use a Hashmap, 
+// number of goals the team scored, and the total number of goals the team
+// conceded. One approach to build the scores table is to use a Hashmap.
+// The solution is partially written to use a Hashmap,
 // complete it to pass the test.
 //
 // Make me pass the tests!
 //
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
-
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -40,6 +38,39 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded by team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        if !scores.contains_key(&team_1_name) {
+            scores.insert(
+                team_1_name,
+                Team {
+                    goals_scored: team_1_score,
+                    goals_conceded: team_2_score,
+                },
+            );
+        } else {
+            scores.entry(team_1_name).and_modify(|e| {
+                *e = Team {
+                    goals_scored: team_1_score + e.goals_scored,
+                    goals_conceded: team_2_score + e.goals_conceded,
+                }
+            });
+        }
+        if !scores.contains_key(&team_2_name) {
+            scores.insert(
+                team_2_name,
+                Team {
+                    goals_scored: team_2_score,
+                    goals_conceded: team_1_score,
+                },
+            );
+        } else {
+            scores.entry(team_2_name).and_modify(|e| {
+                *e = Team {
+                    goals_scored: team_2_score + e.goals_scored,
+                    goals_conceded: team_1_score + e.goals_conceded,
+                }
+            });
+        }
     }
     scores
 }
